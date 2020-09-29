f472415089df39adf80c105f6def0ee2b15bec44
John Exarhouleas <exarhouleasjohn@gmail.com>
2020-09-29 15:08:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,15 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  data.find
+  
+  new = {}
+  c = 1
+  data.each do |section|
+    section.find do |name|
+      if name != new
+        new << name.to_sym
+        new[c] => 
+      end
+      c += 1
+    end
+  end
 end
