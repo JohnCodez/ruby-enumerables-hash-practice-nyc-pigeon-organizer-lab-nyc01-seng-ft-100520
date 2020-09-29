0dc989e7e25eb79203d06fd66f14c5e4161b77ff
John Exarhouleas <exarhouleasjohn@gmail.com>
2020-09-29 16:33:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,43 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-  
-  new = {}
-  data.each do |category|
-    category.each do |type|
-      type.each do |element|  
-        new.each do |name|
-          if element != name
-            new << element.to_sym
-          end
-        end
+  pigeon_list = {}
+  data.each do |key, value|
+  value.each do |describe, array| 
+  array.each do |name|
+  if pigeon_list.has_key?(name)
+   if pigeon_list[name].has_key?(key)
+    pigeon_list[name][key] << describe.to_s
+   else
+    pigeon_list[name][key] = [describe.to_s]
+   end
+  else
+  pigeon_list[name] = {key => [describe.to_s]} #describe was symbol before so turn it to String
       end
+     end
     end
   end
-  
-  
-  new.each do |key|
-    c = 0
-    name = key.to_s
-    arr = []
-    data.each do |category|
-      i = 0
-      tarr = []
-      category.each do |type|
-        type.each do |element|
-          if element == name
-            arr << category.to_sym
-            tarr << type
-          end
-        end
-      end
-      [arr[i], tarr].to_h
-      i += 1
-    end
-    [new[c], arr].to_h
-    c += 1
-  end
-  
-  
-  
-  
+  pigeon_list
 end
